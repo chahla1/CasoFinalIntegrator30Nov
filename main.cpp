1924491b370a7a19606bb837db32acfd485c5fcf
chahla1 <cbouchot@myuax.com>
2023-11-30 12:49:41 +0100

    otro commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct ColorConsole
+{
+    static constexpr auto fg_blue = "\033[34m";
+    static constexpr auto bg_white = "\033[47m";
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
+
